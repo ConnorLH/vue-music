f938daf5e5afa83a693df0bf6a9bf4b9c701abea
TS.CTU.liuhuan <liuhuan@travelsky.com>
2018-10-30 17:13:55 +0800

    初始化

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    歌手详情
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus" type="text/stylus">
+
+</style>
